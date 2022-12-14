9d1c9eb6bae00bf22ed3915c19662c3e34ac2323
Yuri <yuriabaldini@gmail.com>
2022-12-14 10:22:09 -0300

    created docker configuration and django project

---
+++ scripts/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+ls -la /vol/
+ls -la /vol/web
+
+whoami
+
+python manage.py wait_for_db
+python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8100
+
+uwsgi --socket :9100 --workers 4 --master --enable-threads --module app.wsgi
+
